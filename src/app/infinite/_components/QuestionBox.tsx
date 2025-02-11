b1c0816f0a31711f0944e4fec799e1c85b311205
Jamie Thomas <jamiewthomas13@gmail.com>
2025-02-11 22:27:25 +0000

    Fetch questions 10 at a time, infinitely

---
+++ src/app/infinite/_components/QuestionBox.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+
+export default function QuestionBox({
+  question,
+  nextQuestionAction,
+}: {
+  question: Question;
+  nextQuestionAction: () => void;
+}) {
+  const { question_text, correct_answer, available_answers } = question;
+
+  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+
+  function handleNextButtonClick() {
+    setSelectedAnswer(null);
+    nextQuestionAction();
+  }
+
+  return (
+    <>
+      <div className="bg-secondary shadow-md rounded-lg p-6 mb-6">
+        <h2 className="text-xl text-primary font-semibold mb-4">
+          {question_text}
+        </h2>
+        <div className="grid grid-cols-2 gap-4">
+          {available_answers.map((answer, index) => (
+            <Button
+              key={answer}
+              variant={selectedAnswer === index ? "default" : "outline"}
+              className="h-20 text-lg"
+              onClick={() => setSelectedAnswer(index)}
+            >
+              {answer}
+            </Button>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between">
+        <Button disabled>Previous</Button>
+        <Button
+          disabled={selectedAnswer === null}
+          onClick={handleNextButtonClick}
+        >
+          Next
+        </Button>
+      </div>
+    </>
+  );
+}
---
+++ src/app/infinite/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import QuestionBox from "./_components/QuestionBox";
+import { useTrivia } from "@/hooks/quizHooks";
+import QuestionBoxSkeleton from "./_components/QuestionBoxSkeleton";
+import { useEffect, useState } from "react";
+
+export default function Infinite() {
+  const { isPending, questions, refetch } = useTrivia(10);
+
+  const [index, setIndex] = useState<number>(0);
+
+  useEffect(() => {
+    if (index === 9) {
+      refetch();
+      setIndex(0);
+    }
+  }, [index, refetch]);
+
+  function handleIncrementIndex() {
+    setIndex((index) => index + 1);
+  }
+
+  return (
+    <div className="max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-6">Infinite Mode</h1>
+      {isPending ? (
+        <QuestionBoxSkeleton />
+      ) : (
+        <QuestionBox
+          question={questions[index]}
+          nextQuestionAction={handleIncrementIndex}
+        />
+      )}
+      <div className="mt-4">
+        <Link href="/">
+          <Button variant="link">Exit Quiz</Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
