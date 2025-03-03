5d7d43c6b57d45f44919550021f4dfc33c890275
Jamie Thomas <jamiewthomas13@gmail.com>
2025-03-03 23:00:40 +0000

    refactor: Move multiple schema files into schema folder

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,7 @@ import { env } from "@/env.mjs";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/lib/db";
-import { user, session, account, verification } from "@/db/auth";
+import { user, session, account, verification } from "@/db/schema/authTables";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
