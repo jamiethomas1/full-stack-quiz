3a6f50bbfde10e8bd2416579648980b9cb78fc20
Jamie Thomas <jamiewthomas13@gmail.com>
2025-03-06 08:42:01 +0000

    chore: Remove unused API route file

--- src/app/api/quiz/infinite/score/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from "next/server";
-
-// use trpc + zod for this. pretty major refactor.
-
-export async function POST(request: NextRequest) {
-  // validate request
-  // insert into db
-  // return response
-}
